71d44b36a7f083c09a2aef54f9fbce7efe3751ef
MikheevRoman <130794493+MikheevRoman@users.noreply.github.com>
2024-10-23 21:44:33 +0300

    Add files via upload

---
+++ src/main/java/just_ai/test/echo_bot/config/TomcatConfig.java
@@ -0,0 +1,15 @@
+package just_ai.test.echo_bot.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public TomcatServletWebServerFactory tomcatFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector -> connector.setURIEncoding("UTF-8"));
+        return factory;
+    }
+}
